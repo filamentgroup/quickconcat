3401175ba0b6043ddffb059bf5d943bde289300f
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-06-24 15:47:46 +1000

    Added basic caching so files aren't read every time the same set is requested.

--- quickconcat.php
+++ quickconcat.php
@@ -2,7 +2,7 @@
 /*
 quickconcat: a simple dynamic concatenator for html, css, and js files
 	Copyright 2012, Scott Jehl, Filament Group, Inc. Dual licensed under MIT and GPLv2
-	*  accepts 2 query strings: 
+	*  accepts 2 query strings:
 		* files (required): a comma-separated list of root-relative file paths
 		* wrap (optional): Enclose each result in an element node with url attribute? False by default.
 */
@@ -13,7 +13,7 @@ $filelist = $_REQUEST[ "files" ];
 $wrap = isset( $_REQUEST[ "wrap" ] );
 
 // Get the filetype and array of files
-if ( ! isset( $filelist ) ){
+if ( ! isset( $filelist ) ) {
 	echo '$files must be specified!';
 	exit;
 }
@@ -25,15 +25,30 @@ $fext = preg_match( '/\.(js|html|css)$/', $files[ 0 ], $match );
 $ftype = $fext ? $match[ 1 ] : "html";
 $type = "text/" . ( $ftype === "js" ? "javascript" : $ftype );
 
-$contents = '';
+$cache_dir =  __DIR__ . '/.quickconcat-cache';
+$cache_name = $cache_dir . '/' . md5( $filelist . ($wrap ? 'wrap' : '') );
 
-// Loop through the files adding them to a string
-foreach ( $files as $file ) {
-	if( preg_match( '/\.(js|html|css)$/', $file ) ){
-		$open = $wrap ? "<entry url=\"". $file . "\">" : "";
-		$close = $wrap ? "</entry>\n" : "";
-		$contents .= $open . file_get_contents($file). $close;
+// See if cached copy exists
+if ( file_exists($cache_name) ) {
+	$contents = file_get_contents($cache_name);
+}
+else {
+	$contents = '';
+
+	// Loop through the files adding them to a string
+	foreach ( $files as $file ) {
+		if ( preg_match( '/\.(js|html|css)$/', $file ) ){
+			$open = $wrap ? "<entry url=\"". $file . "\">" : "";
+			$close = $wrap ? "</entry>\n" : "";
+			$contents .= $open . file_get_contents($file). $close;
+		}
 	}
+
+	// Write out cache file
+	if ( ! is_dir($cache_dir) ) {
+		mkdir($cache_dir);
+	}
+	file_put_contents($cache_name, $contents);
 }
 
 // Set the content type and filesize headers
