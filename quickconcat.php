b9e6311929eced06dd40ed9c9defb703a78aa14c
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-06-24 15:54:26 +1000

    Added nocache flag that will prevent a cache being read or created. (Useful for dynamic HTML being read in.)

--- quickconcat.php
+++ quickconcat.php
@@ -2,15 +2,19 @@
 /*
 quickconcat: a simple dynamic concatenator for html, css, and js files
 	Copyright 2012, Scott Jehl, Filament Group, Inc. Dual licensed under MIT and GPLv2
-	*  accepts 2 query strings:
+	*  accepts 3 query strings:
 		* files (required): a comma-separated list of root-relative file paths
 		* wrap (optional): Enclose each result in an element node with url attribute? False by default.
+		* nocache (optional): Do not read from or create a cache file? False by default.
 */
 // List of files, comma-separated paths
 $filelist = $_REQUEST[ "files" ];
 
 // Enclose each result in an element node with url attribute?
 $wrap = isset( $_REQUEST[ "wrap" ] );
+
+// Do not read from or create a cache file?
+$nocache = isset( $_REQUEST[ "nocache" ] );
 
 // Get the filetype and array of files
 if ( ! isset( $filelist ) ) {
@@ -29,7 +33,7 @@ $cache_dir =  __DIR__ . '/.quickconcat-cache';
 $cache_name = $cache_dir . '/' . md5( $filelist . ($wrap ? 'wrap' : '') );
 
 // See if cached copy exists
-if ( file_exists($cache_name) ) {
+if ( ! $nocache && file_exists($cache_name) ) {
 	$contents = file_get_contents($cache_name);
 }
 else {
@@ -44,11 +48,13 @@ else {
 		}
 	}
 
-	// Write out cache file
-	if ( ! is_dir($cache_dir) ) {
-		mkdir($cache_dir);
+	if ( ! $nocache ) {
+		// Write out cache file
+		if ( ! is_dir($cache_dir) ) {
+			mkdir($cache_dir);
+		}
+		file_put_contents($cache_name, $contents);
 	}
-	file_put_contents($cache_name, $contents);
 }
 
 // Set the content type and filesize headers
