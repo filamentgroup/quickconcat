e24ff7c7e0cd1fe468d1ef57fe5007d28420a8e0
Markus Staab <markus.staab@redaxo.de>
2012-09-01 15:53:44 +0200

    fixed output

--- test/testServer.php
+++ test/testServer.php
@@ -8,11 +8,7 @@ use Symfony\Component\Process\Process;
 $process = new Process('php -S localhost:8888 -t '. realpath(dirname(__DIR__)));
 $process->setTimeout(1800);
 $process->run(function ($type, $buffer) {
-    if ('err' === $type) {
-        echo 'ERR > '.$buffer;
-    } else {
-        echo 'OUT > '.$buffer;
-    }
+    echo '> '.$buffer;
 });
 if (!$process->isSuccessful()) {
     throw new RuntimeException($process->getErrorOutput());
