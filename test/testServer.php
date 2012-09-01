8c1f195e9afb4273b362e5d98a643a2df71f24f2
Markus Staab <markus.staab@redaxo.de>
2012-09-01 15:54:58 +0200

    fixed server

--- test/testServer.php
+++ test/testServer.php
@@ -7,9 +7,8 @@ use Symfony\Component\Process\Process;
 // bootup a server for running the tests
 $process = new Process('php -S localhost:8888 -t '. realpath(dirname(__DIR__)));
 $process->setTimeout(1800);
-$process->run(function ($type, $buffer) {
-    echo '> '.$buffer;
-});
+$process->run();
+
 if (!$process->isSuccessful()) {
     throw new RuntimeException($process->getErrorOutput());
 }
