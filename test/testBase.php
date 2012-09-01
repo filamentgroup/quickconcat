e4f53a3203088a7f2068f5dc741b0b81a69df97e
Markus Staab <markus.staab@redaxo.de>
2012-09-01 15:46:30 +0200

    fixed path to files

--- test/testBase.php
+++ test/testBase.php
@@ -1,6 +1,6 @@
 <?php
 
-class quickconcat_basetest extends PHPUnit_Framework_TestCase
+abstract class quickconcat_basetest extends PHPUnit_Framework_TestCase
 {	
 	protected function get($url) {
 		return file_get_contents('http://localhost:8888/'. $url);
