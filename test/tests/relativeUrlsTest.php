041d2b8df928c7f1386f012a9cb947d0cd679970
Markus Staab <maggus.staab@googlemail.com>
2012-10-25 17:45:02 +0200

    fixed regex problems and do the whole stuff in just on replace. added unit test.

---
+++ test/tests/relativeUrlsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once dirname(__DIR__). '/testBase.php';
+
+class relativeUrls extends quickconcat_basetest
+{		
+	public function testUrls()
+	{
+		$expected = '@font-face { 
+	font-family: Route; src: url(//test/assets/Route.woff);
+}
+
+div.absolute {
+	background:        url(\'/woaar/yes.png\') repeat-y bottom;
+}
+span.absolute {
+	background:        url(/pics/item.jpeg) repeat-y bottom;
+}
+div.absolute .abc {
+	background:        url("/img/a/img.png") repeat-y bottom;
+}
+div.relative {
+	background:        url(//test/assets/../img/layout/warehouse.png) repeat-y bottom;
+}';
+		$this->assertEquals(
+			$this->get('quickconcat.php?files=test/assets/url.css'),
+			$expected);
+	}	
+}
