e26ab405d14db686bbe1fa9b0338e074ecbbd610
Jeffrey Lembeck <jlembeck@gmail.com>
2012-10-30 09:42:30 -0700

    EOF should have a new line

--- test/tests/concatTest.php
+++ test/tests/concatTest.php
@@ -7,28 +7,28 @@ class quickconcat extends quickconcat_basetest
 	public function testBasic()
 	{
 		$this->assertEquals(
-			$this->get('quickconcat.php?files=test/assets/a.html'),
-			'<h1>hello</h1>');
+			'<h1>hello</h1>',
+			$this->get('quickconcat.php?files=test/assets/a.html'));
 	}
 	
 	public function testConcatHtml()
 	{
 		$this->assertEquals(
-			$this->get('quickconcat.php?files=test/assets/a.html,test/assets/b.html,test/assets/c.html'),
-			'<h1>hello</h1><p>world</p><h3>quickconcat</h3>');
+			'<h1>hello</h1><p>world</p><h3>quickconcat</h3>',
+			$this->get('quickconcat.php?files=test/assets/a.html,test/assets/b.html,test/assets/c.html'));
 	}
 	
 	public function testConcatCss()
 	{
 		$this->assertEquals(
-			$this->get('quickconcat.php?files=test/assets/a.css,test/assets/b.css,test/assets/c.css'),
-			'h1 { font-weight: bold }p { font-weight: bold }h3 { font-weight: bold }');
+			'h1 { font-weight: bold }p { font-weight: bold }h3 { font-weight: bold }',
+			$this->get('quickconcat.php?files=test/assets/a.css,test/assets/b.css,test/assets/c.css'));
 	}
 	
 	public function testConcatJs()
 	{
 		$this->assertEquals(
-			$this->get('quickconcat.php?files=test/assets/a.js,test/assets/b.js,test/assets/c.js'),
-			'var a = "a";var b = "b";var c = "c";');
+			"var a = \"a\";\nvar b = \"b\";\nvar c = \"c\";\n",
+			$this->get('quickconcat.php?files=test/assets/a.js,test/assets/b.js,test/assets/c.js'));
 	}
 }
