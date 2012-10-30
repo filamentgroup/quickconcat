fbd6caa3aec92cd9e1d07b2fe50766f113b53104
Jeffrey Lembeck <jlembeck@gmail.com>
2012-10-30 09:59:46 -0700

    Modifying tests slightly with better spacing

--- test/tests/relativeUrlsTest.php
+++ test/tests/relativeUrlsTest.php
@@ -3,27 +3,29 @@
 require_once dirname(__DIR__). '/testBase.php';
 
 class relativeUrls extends quickconcat_basetest
-{		
+{
 	public function testUrls()
 	{
-		$expected = '@font-face { 
+		$expected =
+"@font-face {
 	font-family: Route; src: url(//test/assets/Route.woff);
 }
 
 div.absolute {
-	background:        url(\'/woaar/yes.png\') repeat-y bottom;
+	background:url('/woaar/yes.png') repeat-y bottom;
 }
 span.absolute {
-	background:        url(/pics/item.jpeg) repeat-y bottom;
+	background:url(/pics/item.jpeg) repeat-y bottom;
 }
 div.absolute .abc {
-	background:        url("/img/a/img.png") repeat-y bottom;
+	background:url(\"/img/a/img.png\") repeat-y bottom;
 }
 div.relative {
-	background:        url(//test/assets/../img/layout/warehouse.png) repeat-y bottom;
-}';
-		$this->assertEquals(
-			$this->get('quickconcat.php?files=test/assets/url.css'),
-			$expected);
-	}	
+	background:url(//test/assets/../img/layout/warehouse.png) repeat-y bottom;
+}
+";
+	$this->assertEquals(
+		$expected,
+		$this->get('quickconcat.php?files=test/assets/url.css'));
+}
 }
